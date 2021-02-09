0a3b00830611e24df5e522edd7ed0c0c9bf8bbd8
Kunal Ashar <kunalashar@Kunals-MacBook-Pro.local>
2021-02-09 22:23:02 +0530

    operator precedence example

---
+++ js_fundamentals_part_1/operator_precedence/script.js
@@ -0,0 +1,12 @@
+const currentYear = 2022;
+
+const ageJonas = currentYear - 1991;
+const ageKelvin = currentYear - 1993;
+
+console.log(currentYear - 1991 > currentYear - 1993);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y); // 10 10
