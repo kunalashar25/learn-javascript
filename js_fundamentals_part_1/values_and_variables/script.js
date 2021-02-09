3a2c661d65709354e82fb36ae2d6669e00f7e429
Kunal Ashar <kunalashar@Kunals-MacBook-Pro.local>
2021-02-09 17:29:31 +0530

    varaible naming conventions

---
+++ js_fundamentals_part_1/values_and_variables/script.js
@@ -0,0 +1,15 @@
+let firstName = 'Jonas';
+console.log(firstName);
+
+let PI = 3.1415;
+
+// Variable naming convention
+// 1. Camel Casing → preferred type
+// 2. Using underscore → first_name
+// 3. Cannot start with number
+// 4. Can use numbers, letters, _ , and $
+// 5. Cannot use reserved JS keyword like new, function, etc.
+// 6. name is a reserved keyword but it is still legal to use → Recommended not to use
+// 7. Do not write varaible name with upper case characters → allowed but best practice is not to use it
+// 8. Constants should be written in all upper case
+// 9. Write meaningful names for the variables.
