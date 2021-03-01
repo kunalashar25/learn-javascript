305dbdcd0a8ed5f512e2f34675cc3b9b1cacb40e
Kunal Ashar <kunalashar@Kunals-MacBook-Pro.local>
2021-03-01 21:13:32 +0530

    introduction to objects in js

---
+++ assignment/js_fundamentals_part_2/introduction_to_objects/assignment.js
@@ -0,0 +1,9 @@
+const myCountry = {
+    country: 'India',
+    capital: 'New Delhi',
+    language: 'Hindi',
+    population: 1500,
+    neighbours: ['Nepal', 'Bhutan']
+};
+
+console.log(myCountry); // {country: "India", capital: "New Delhi", language: "Hindi", population: 1500, neighbours: Array(2)}
---
+++ js_fundamentals_part_2/introduction_to_objects/script.js
@@ -0,0 +1,17 @@
+// Object is data structure in JS
+
+// Object literal syntax
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Peter',
+    age: 2021 - 1991,
+    job: 'teacher'
+};
+
+// Each key inside object is called as property
+// jonas object has 4 properties
+
+console.log(jonas); // {firstName: "Jonas", lastName: "Peter", age: 30, job: "teacher"}
+console.log(jonas.firstName); // Jonas
+
+// order of properties does not matter in objects
